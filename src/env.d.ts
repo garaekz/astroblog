498f602369185c56c892af9af66a1d19446e5d28
David Garay <garaekz@gmail.com>
2024-05-03 14:40:12 -0700

    Auth middleware

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,16 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_KEY: string
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_KEY: string;
 }
 
-    interface ImportMeta {
-    readonly env: ImportMetaEnv
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    email: String;
+  }
 }
--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,10 +1,27 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+  const form = await request.formData();
+  const email = form.get("email")?.toString();
+  const password = form.get("password")?.toString();
+  const provider = form.get("provider")?.toString();
+
+  if (provider) {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: provider as Provider,
+      options: {
+        redirectTo: import.meta.env.CALLBACK_URL,
+      },
+    });
+
+    if (error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return redirect(data.url);
+  }
 
   if (!email || !password) {
     return new Response("Email and password are required", { status: 400 });
@@ -21,10 +38,15 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
 
   const { access_token, refresh_token } = data.session;
   cookies.set("sb-access-token", access_token, {
+    sameSite: "strict",
     path: "/",
+    secure: true,
   });
   cookies.set("sb-refresh-token", refresh_token, {
+    sameSite: "strict",
     path: "/",
+    secure: true,
   });
+
   return redirect("/dashboard");
 };
