e1078d3ad6852997f67adc936506a9dffec05f4d
David Garay <garaekz@gmail.com>
2024-05-04 09:39:59 -0700

    Basic top and sidenav

---
+++ src/components/dashboard/Navigation.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { getCurrentInstance, provide } from "vue";
+import Navbar from "./Navbar.vue";
+import Sidebar from "./Sidebar.vue";
+import SidebarBackdrop from "./SidebarBackdrop.vue";
+import pinia from "@plugins/pinia";
+
+const props = defineProps({
+  user: Object,
+});
+
+const app = getCurrentInstance().appContext.app;
+app.use(pinia);
+
+provide("user", props.user);
+</script>
+<template>
+  <div>
+    <Navbar />
+    <Sidebar />
+    <SidebarBackdrop />
+  </div>
+</template>
---
+++ src/components/dashboard/SidebarBackdrop.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useDrawerStore } from "@stores/drawer";
+import { storeToRefs } from "pinia";
+
+const store = useDrawerStore();
+const { isDrawerOpen } = storeToRefs(store);
+const { toggleDrawer } = store;
+</script>
+<template>
+  <div
+    v-if="isDrawerOpen"
+    @click="toggleDrawer"
+    class="h-screen w-full absolute bg-black/30 overflow-hidden"
+  ></div>
+</template>
